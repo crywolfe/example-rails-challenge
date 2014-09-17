0dd5c5b7c33395f78e923cfa08e83f0d51811b75
crywolfe <gwolfe2007@gmail.com>
2014-09-17 11:45:23 -0400

    finished testing

--- spec/features/index.html.erb_spec.rb
+++ spec/features/index.html.erb_spec.rb
@@ -3,19 +3,18 @@ require 'rails_helper'
 
 describe "charges/index.html.erb" do
   before :each do
-    visit '/charges/index'
+    visit '/charges'
   end
 
   context "three lists on the screen" do
     it "should find Successful" do
-      expect(page).to assert_text("SUCCESSFUL")
+      expect(page).to have_content("SUCCESSFUL")
     end
     it "should find Disputed" do
       expect(page).to have_content("DISPUTED")
     end
     it "should find Failed" do
-      page.find('h1', :text => 'FAILED')
-      # expect(page).to assert_text("FAILED")
+      expect(page).to have_content("DISPUTED")
     end
 
   end
