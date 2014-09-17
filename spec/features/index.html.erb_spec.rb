04c5911c4df46a33be7fb03a1c5afe16b9fc6f0a
crywolfe <gwolfe2007@gmail.com>
2014-09-17 07:54:08 -0400

    clean up charge_spec test

--- spec/features/index.html.erb_spec.rb
+++ spec/features/index.html.erb_spec.rb
@@ -2,17 +2,18 @@ require 'spec_helper'
 require 'rails_helper'
 
 describe "charges/index.html.erb" do
+  before :each do
+    visit '/charges/index'
+  end
+
   context "three lists on the screen" do
     it "should find Successful" do
-      visit '/charges/index'
       expect(page).to assert_text("SUCCESSFUL")
     end
     it "should find Disputed" do
-      visit '/charges/index'
-      expect(page).to assert_text("DISPUTED")
+      expect(page).to have_content("DISPUTED")
     end
     it "should find Failed" do
-      visit '/charges/index'
       page.find('h1', :text => 'FAILED')
       # expect(page).to assert_text("FAILED")
     end
